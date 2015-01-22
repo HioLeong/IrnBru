bbb77849940f444f60ab75800eb9d980d4b8bebf
hioleong <hiowengleong@gmail.com>
2015-01-22 09:39:58 +0000

    [Template] Added Base and changed the article trainer template

--- IrnBruMvc/topics_trainer/urls.py
+++ IrnBruMvc/topics_trainer/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from topics_trainer import views
 
 urlpatterns = patterns('', 
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<article_index>\d+)/$', views.article_choice, name='article_choice')
 )
+
--- IrnBruMvc/topics_trainer/views.py
+++ IrnBruMvc/topics_trainer/views.py
@@ -4,7 +4,11 @@ from django.template import RequestContext, loader
 from topics_trainer.models import Article
 
 def index(request):
-    article = Article.objects.all()[0]
+    return HttpResponse('/topics_trainer/index for articles')
+
+def article_choice(request, article_index):
+
+    article = Article.objects.all()[int(article_index)]
     template = loader.get_template('article_choice.html')
     context = RequestContext(request, {
         'article': article
