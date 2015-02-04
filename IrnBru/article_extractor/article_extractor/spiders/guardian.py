cb22b46e29b0351b922b11ab907be805933e1477
hioleong <hiowengleong@gmail.com>
2015-02-04 16:15:33 +0000

    [Crawl] Adapters implemented

--- IrnBru/article_extractor/article_extractor/spiders/guardian.py
+++ IrnBru/article_extractor/article_extractor/spiders/guardian.py
@@ -1,10 +1,10 @@
 import scrapy
+import article_extractor
 from scrapy.selector import Selector
 from scrapy.http import Request
 from article_extractor.items import Article
-from article_extractor.spiders import ArticleSpider
 
-class GuardianSpider(ArticleSpider):
+class GuardianSpider(article_extractor.spiders.article_spider.ArticleSpider):
     name = "guardian"
     allowed_domains = ["theguardian.com","google.com","google.co.uk"]
     start_urls = [
@@ -12,11 +12,8 @@ class GuardianSpider(ArticleSpider):
         "https://www.google.com/search?q=site%3Atheguardian.com+scottish+devolution&num=1000&tbs=cdr%3A1%2Ccd_min%3A%2Ccd_max%3A9%2F17%2F2014"
     ]
 
-    xpaths = { date: '//span[@class="date"]/text()', 
-            title: '//h1[@class="content__headline js-score"]/text()', 
-            body: '//div[@class="content__article-body from-content-api js-article__body"]/p/text()' 
+    selections = { 
+            'date': '//span[@class="date"]/text()', 
+            'title': '//h1[@class="content__headline js-score"]/text()', 
+            'body': '//div[@class="content__article-body from-content-api js-article__body"]/p/text()' 
             }
-
-    def __init__(self):
-        self.xpaths = xpaths
-
