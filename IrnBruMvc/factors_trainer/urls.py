56e9c373fd59b4969b196c8e5419f11a8a802ff8
hioleong <hiowengleong@gmail.com>
2015-03-03 22:38:17 +0000

    [Factors] Added reporting (blank)

--- IrnBruMvc/factors_trainer/urls.py
+++ IrnBruMvc/factors_trainer/urls.py
@@ -4,6 +4,7 @@ from factors_trainer import views
 
 urlpatterns = patterns('factors_trainer', 
     url(r'update/$', views.index, name='index'),
+    url(r'report/$', views.report, name='index'),
     url(r'get_factor/(?P<topic_name>\w+)/$', views.get_factors, name='get_factors'),
     url(r'update_factor/(?P<factor_id>.*)/$', views.update_factor, name='update_factor'),
 )
--- IrnBruMvc/factors_trainer/views.py
+++ IrnBruMvc/factors_trainer/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.template import RequestContext, loader
@@ -13,6 +14,13 @@ def get_factors(request, topic_name):
     context = RequestContext(request, {
         'factors': factors,
         'topic_name': topic_name.title()
+        })
+    return HttpResponse(template.render(context))
+
+def report(request):
+    template = loader.get_template('report.html')
+    context = RequestContext(request, {
+        'content': 'content'
         })
     return HttpResponse(template.render(context))
 
