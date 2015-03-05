df982088ca11801f681100a952674735cc28e9fc
hioleong <hiowengleong@gmail.com>
2015-03-05 10:24:09 +0000

    [Classifier] Classifying article topics

--- IrnBruMvc/classifier/topics_classifier.py
+++ IrnBruMvc/classifier/topics_classifier.py
@@ -1,4 +1,5 @@
 import string
+import nltk
 from nltk import NaiveBayesClassifier
 from nltk.featstruct import FeatStruct
 
@@ -6,7 +7,7 @@ from article_summary.word_op import *
 from topics_trainer.models import Article
 from topics_editor.models import Topic
 
-def __train_set__():
+def get_train_set():
     label_topic = []
     for topic in Topic.objects.all():
         topic_choices = get_choices_of_topic(topic.id)
@@ -15,6 +16,9 @@ def __train_set__():
             label_topic.append((article, topic.topic))
     train_set = [(topic_features(n), topic) for (n,topic) in label_topic]
     return train_set
+
+def get_test_set():
+    label = topic
 
 def get_word_occurence(article):
     body_toks = aggregate_list_of_lists([word_tokenize(b) for b in article.body])
@@ -28,17 +32,20 @@ def topic_features(article):
     word_occurence = get_word_occurence(article)
     feature_list = FeatStruct(word_occurence)
     feature_list.freeze()
-    return FeatStruct(word_occurence=feature_list)
-#return { 'word_occurence': word_occurence }
+    feature = FeatStruct(word_occurence=feature_list)
+    feature.freeze()
+    return feature
 
-def train_topic_classifier():
-    label_topic = []
-    for topic in Topic.objects.all():
-        topic_choices = get_choices_of_topic(topic.id)
-        for choice in topic_choices:
-            article = get_article_from_choice(choice)
-            label_topic.append((article, topic.topic))
-    train_set = [(topic_features(n), topic) for (n,topic) in label_topic]
+def train_topic_classifier(train_set):
     classifier = NaiveBayesClassifier.train(train_set)
     return classifier
 
+def get_classifier_accuracy(classifier, train_set):
+    for item in train_set:
+        print item[0].frozen()
+    print nltk.classify.accuracy(classifier, train_set)
+
+def test():
+    t_set = get_train_set()
+    classifier = train_topic_classifier(t_set)
+    get_classifier_accuracy(classifier, t_set)
--- IrnBruMvc/shell.sh
+++ IrnBruMvc/shell.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python manage.py shell
+from classifier.topics_classifier import *
