ca217960f9e7ad2358022153512ab5d28f5f14c8
hioleong <hiowengleong@gmail.com>
2015-03-06 04:49:32 +0000

    [Classifier] Added factors classifier

---
+++ IrnBruMvc/classifier/factors_classifier.py
@@ -0,0 +1,10 @@
+import string
+from nltk import NaiveBayesClassifier
+from nltk.featstruct import FeatStruct
+
+def factor_features(factor):
+    #TODO: Sentence patterns
+    return
+
+def train_factor_classifier():
+    return
