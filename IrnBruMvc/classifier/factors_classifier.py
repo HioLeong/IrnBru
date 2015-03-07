606641389dbd02a9e72fb58f0c4a84e8ccd5bf46
hioleong <hiowengleong@gmail.com>
2015-03-07 05:45:00 +0000

    [Classifier] Added factors feature from POS pattern

--- IrnBruMvc/classifier/factors_classifier.py
+++ IrnBruMvc/classifier/factors_classifier.py
@@ -2,18 +2,25 @@ import string
 from nltk import NaiveBayesClassifier
 from nltk.featstruct import FeatStruct
 
+from factors_trainer.factor import *
 
 class FactorsClassifier():
     def __init__(self):
         self.train_set = self.get_train_set()
         self.classifier = train_factor_classifier(self.train_set)
 
+    def __get_pos_pattern_from_factor__(self, factor):
+        sentence = factor.factor
+        pos_tags = get_pos_tag_of_sentence(sentence)
+        pos_pattern = aggregate_pos_tags(pos_tags)
+        return pos_pattern
+
     def get_train_set(self):
         return []
 
     def factor_features(self, factor):
-        feat
-        return
+        pos_pattern = self.__get_pos_pattern_from_factor__(factor)
+        return { 'pos_pattern': pos_pattern }
 
     def train_factor_classifier(self, train_set):
         classifier = NaiveBayesClassifier
