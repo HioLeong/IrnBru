8992ddd4cf20a12a5732ee938ec501d251243c74
hioleong <hiowengleong@gmail.com>
2015-03-08 05:43:32 +0000

    [Classifier] Get training set impl

--- IrnBruMvc/classifier/factors_classifier.py
+++ IrnBruMvc/classifier/factors_classifier.py
@@ -2,11 +2,14 @@ import string
 from nltk import NaiveBayesClassifier
 from nltk.featstruct import FeatStruct
 
+from topics_editor.models import Topic
 from factors_trainer.factor import *
+from factors_trainer.models import Factor
 
 class FactorsClassifier():
-    def __init__(self):
-        self.train_set = self.get_train_set()
+    def __init__(self, topic_name):
+        topic = Topic.objects.get(topic=topic_name)
+        self.train_set = self.get_train_set(topic)
         self.classifier = train_factor_classifier(self.train_set)
 
     def __get_pos_pattern_from_factor__(self, factor):
@@ -15,13 +18,17 @@ class FactorsClassifier():
         pos_pattern = aggregate_pos_tags(pos_tags)
         return pos_pattern
 
-    def get_train_set(self):
-        return []
+    def get_train_set(self, topic):
+        topic_factors = Factor.objects.filter(topic=topic)
+        # Does not take into consideration Neutrals
+        sentiment_factors  = [factor for factor in topic_factors if factor.sentiment in ['Yes','No']]
+        train_set = [(factor,factor.sentiment) for factor in sentiment_factors]
+        return train_set
 
     def factor_features(self, factor):
         pos_pattern = self.__get_pos_pattern_from_factor__(factor)
         return { 'pos_pattern': pos_pattern }
 
     def train_factor_classifier(self, train_set):
-        classifier = NaiveBayesClassifier
-        return
+        classifier = NaiveBayesClassifier(train_set)
+        return classifier
