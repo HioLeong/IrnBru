4d78b67aef0142eb4bf877233615e4daeed7e1ff
hioleong <hiowengleong@gmail.com>
2015-03-12 22:29:23 +0100

    [Interface] Dashboard interfaces

--- IrnBruMvc/front_page/views.py
+++ IrnBruMvc/front_page/views.py
@@ -8,4 +8,6 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def dashboard(request):
-    # TODO: Implement
+    template = loader.get_template('dashboard.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
