90ac2c14f57fb1283ca403fb8b649734c745a3dc
hioleong <hiowengleong@gmail.com>
2015-03-15 15:29:14 +0100

    [Interface] Form URL for classifier

--- IrnBruMvc/classifier/urls.py
+++ IrnBruMvc/classifier/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from classifier import views
 
 urlpatterns = patterns('classifier', 
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'classify_article/(?P<article_type>\w+)/$', views.classify_article, name='classify_article')
 )
 
--- IrnBruMvc/classifier/views.py
+++ IrnBruMvc/classifier/views.py
@@ -6,3 +6,6 @@ def index(request):
     template = loader.get_template('factors_classification.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
+
+def classify_article(request, article_type):
+    return HttpResponse(article_type)
