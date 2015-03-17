0a7a4947bf53f03127a46384f736bb54187ab979
hioleong <hiowengleong@gmail.com>
2015-03-17 16:01:01 +0100

    [Dashboard] topics count

--- IrnBruMvc/front_page/urls.py
+++ IrnBruMvc/front_page/urls.py
@@ -4,6 +4,7 @@ from front_page import views
 
 urlpatterns = patterns('front_page', 
     url(r'^$', views.index, name='index'),
-    url(r'^dashboard/$', views.dashboard, name='dashboard')
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^dashboard/topics_articles_count/$', views.get_topics_articles_count, name='topics_articles_count'),
 )
 
--- IrnBruMvc/front_page/views.py
+++ IrnBruMvc/front_page/views.py
@@ -2,6 +2,11 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from topics_trainer.models import Article, Choice
+from topics_editor.models import Topic
+
+import json
+
 def index(request):
     template = loader.get_template('index.html')
     context = RequestContext(request, {})
@@ -11,3 +16,11 @@ def dashboard(request):
     template = loader.get_template('dashboard.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
+
+def get_topics_articles_count(request):
+    data = []
+    topics  = Topic.objects.all()
+    for topic in topics:
+        topic_count = Choice.objects.filter(topic__contains=topic.id).count()
+        data.append((topic.topic, topic_count))
+    return HttpResponse(json.dumps(data), content_type='application/json')
