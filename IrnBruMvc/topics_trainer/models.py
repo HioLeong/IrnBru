0c878a6df70bb4497d5a112a9654ddc476baefc9
hioleong <hiowengleong@gmail.com>
2015-03-26 04:45:34 +0100

    [Topics] Added source to articles list

--- IrnBruMvc/topics_trainer/models.py
+++ IrnBruMvc/topics_trainer/models.py
@@ -4,6 +4,7 @@ from djangotoolbox.fields import ListField, EmbeddedModelField
 class Article(models.Model):
     title = models.CharField(max_length=200)
     body = ListField(models.CharField(models.TextField()))
+    source = models.CharField(max_length=200)
 
 class Choice(models.Model):
     choice = models.ForeignKey(Article)
