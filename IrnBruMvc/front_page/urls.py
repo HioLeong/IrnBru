9c010a98d9617a235b0e0729399d95a2e405eb18
hioleong <hiowengleong@gmail.com>
2015-04-02 06:19:44 +0200

    [Interface] Added data tree interface

--- IrnBruMvc/front_page/urls.py
+++ IrnBruMvc/front_page/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('front_page',
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^dashboard/topics_articles_count/$', views.get_topics_articles_count, name='topics_articles_count'),
     url(r'^dashboard/factors_count/$', views.get_factors_count, name='factors_count'),
+    url(r'^dashboard/get_all_data/$', views.get_all_data, name='get_all_data'),
 )
 
--- IrnBruMvc/front_page/views.py
+++ IrnBruMvc/front_page/views.py
@@ -41,3 +41,32 @@ def get_factors_count(request):
         factors_count = Factor.objects.exclude(sentiment='').filter(topic=topic).count()
         data.append({'label':topic.topic, 'value': factors_count})
     return HttpResponse(json.dumps(data), content_type='application/json')
+
+def crop_sent_if_over_n(sent,n=150):
+    if len(sent) > n:
+        return sent[:n] + '...'
+    else:
+        return sent
+
+def get_sentiments_tree_from_factors(factors):
+    sentiments = ['Yes', 'No', 'Neutral']
+    sentiment_tree = []
+    for s in sentiments:
+        root = {'name':s }
+        sentiment_factor = [f for f in factors if f.sentiment == s]
+        print sentiment_factor
+        root['children'] = [{'name':crop_sent_if_over_n(f.factor,150),'sentiment':s} for f in sentiment_factor]
+        sentiment_tree.append(root)
+    return sentiment_tree
+
+def get_all_data(request):
+    tree = { 'name':'data' }
+    tree_child = []
+    topics = Topic.objects.all()
+    for topic in topics:
+        topic_root = { 'name': topic.topic }
+        factors = Factor.objects.filter(topic=topic)
+        topic_root['children'] = get_sentiments_tree_from_factors(factors)
+        tree_child.append(topic_root)
+    tree['children'] = tree_child
+    return HttpResponse(json.dumps(tree), content_type='application/json')
